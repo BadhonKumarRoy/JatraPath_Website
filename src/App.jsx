e7d4ca27b321cecbcd172984b5d3cb61840db1e2
BadhonKumarRoy <badhonroy957@gmail.com>
2026-03-18 01:35:23 +0600

    Moved V0.1 to a new new folder and created react folders for V1.0

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Home from './pages/home.jsx'
+import './styles/App.css'
+
+function App() {
+
+  return (
+    <>
+      <p><strong>JatraPath</strong></p>
+      <Home />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is a simple home page component.</p>
+      <a href="/JatraPath_Website/JatraPath_V0.1/old.html">Go to First version of JatraPath</a>
+    </div>
+  );
+};
+
+export default Home;
